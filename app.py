a0812cc8895e1e726b08f1c7a8efe71bebc89826
Vivek Kumar <32337899+vickytilotia@users.noreply.github.com>
2022-01-28 17:08:08 +0530

    show, delete completed

--- app.py
+++ app.py
@@ -1,19 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime
+
 
 app = Flask(__name__)
+# initializing database
+app.config['SQLALCHEMY_DATABASE_URI'] ="sqlite:///flask_todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =False
+db = SQLAlchemy(app)
 
-@app.route("/")
+class ToDo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(600), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} -{self.title}"
+
+@app.route("/", methods =[ 'GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    if request.method == "POST":
+        title = request.form['title']
+        desc = request.form['desc']
+
+        todo = ToDo(title = title, desc= desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo =ToDo.query.all()
+    return render_template('index.html', allTodo=allTodo)
     # return "<p>Hello, World!</p>"
 
-@app.route('/this')
-def index():
+@app.route('/update')
+def update():
+    allTodo =ToDo.query.all()
+    print(allTodo)
     return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'welcome'
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo =ToDo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
 
 if __name__ =="__main__":
     app.run(debug=True)
